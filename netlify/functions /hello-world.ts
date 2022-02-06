8c6f88dfeceeae776fccb781b25b60e7a9945b1f
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-02-06 12:52:43 -0600

    first cron job

--- netlify/functions /hello-world.ts
+++ netlify/functions /hello-world.ts
@@ -1,6 +1,14 @@
-export const handler = async () => {
+import { schedule } from '@netlify/functions';
+
+
+
+ const handlerFn = async () => {
     return {
         statusCode: 200,
         body: 'howdy',
     }
 }
+
+
+
+export const handler = schedule('* * * * *', handlerFn);
